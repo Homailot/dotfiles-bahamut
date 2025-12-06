abf0589df61925922ee08d683de9a1a8cd1bfc07
Nuno Alves <nyansword@gmail.com>
2025-12-06 11:46:55 +0000

    Lazy load toggleterm

--- .config/nvim/lua/nunoa/lazy/toggleterm.lua
+++ .config/nvim/lua/nunoa/lazy/toggleterm.lua
@@ -1,3 +1,9 @@
 return {
-	{ "akinsho/toggleterm.nvim", version = "*", opts = { open_mapping = [[<c-->]] } },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		opts = { open_mapping = [[<c-->]] },
+		keys = { "<c-->", "<cmd>ToggleTerm</cmd>" },
+		lazy = true,
+	},
 }
