fc09acf3db29827af4d937f6e53a1a240bbea088
Nuno Alves <nyansword@gmail.com>
2026-05-19 17:58:08 +0100

    Reorder virtual monitor enablement

--- .config/sunshine/scripts/configure_virtual_monitor.sh
+++ .config/sunshine/scripts/configure_virtual_monitor.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-hyprctl eval 'hl.monitor({output = "DP-1", disabled = true })'
-hyprctl eval 'hl.monitor({output = "DP-2", disabled = true })'
-
 hyprctl output create headless headless-3 
 
 start='hl.monitor({output = "headless-3", mode = "'
@@ -10,6 +7,9 @@ mode="${SUNSHINE_CLIENT_WIDTH}x${SUNSHINE_CLIENT_HEIGHT}@${SUNSHINE_CLIENT_FPS}"
 end='", position = "auto", scale = 1, bitdepth = 10, cm = "hdr", disabled = false })'
 
 hyprctl eval "$start$mode$end"
+
+hyprctl eval 'hl.monitor({output = "DP-1", disabled = true })'
+hyprctl eval 'hl.monitor({output = "DP-2", disabled = true })'
 
 hyprctl dispatch 'hl.dsp.focus({ monitor = "headless-3" })'
 hyprctl dispatch 'hl.dsp.focus({ workspace = 9, on_current_monitor = true })'
